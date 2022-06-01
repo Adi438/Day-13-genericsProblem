4000ccd5669a3c359bba578f02307432c6076e10
Adi438 <mishraaaditya54@gmail.com>
2022-06-01 20:31:14 +0530

    max string code

--- Genericsproblem/Program.cs
+++ Genericsproblem/Program.cs
@@ -7,13 +7,13 @@ namespace TestMaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Maximum Number From Three Float Number\n");
-            Console.Write("Enter Num1 : ");
-            double num1 = double.Parse(Console.ReadLine());
-            Console.Write("Enter Num2 : ");
-            double num2 = double.Parse(Console.ReadLine());
-            Console.Write("Enter Num3 : ");
-            double num3 = double.Parse(Console.ReadLine());
-            double resut = MaxFloatNumber.MaximumFloatNumber(num1, num2, num3);
+            //Console.Write("Enter Num1 : ");
+            //string num1 = (Console.ReadLine());
+            //Console.Write("Enter Num2 : ");
+            //string num2 = (Console.ReadLine());
+            //Console.Write("Enter Num3 : ");
+            //string num3 = (Console.ReadLine());
+            string resut = MaxString.MaximumStringNumber("Praful", "Mayur", "Karan");
             Console.WriteLine(resut);
         }
     }
